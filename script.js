492b0b31d9553db5409ea913846d98d3285e421c
evndroo <evandro.slipjr@gmail.com>
2019-11-15 23:36:51 -0300

    armazenando contatos localmente, adicionando e apagando usuários, ordenando lista, validando formulário, filtrando por qualquer categoria

---
+++ script.js
@@ -0,0 +1,44 @@
+angular.module("listaTelefonica",[])
+angular.module("listaTelefonica").controller("TelefonicaCTRL", function($scope){
+    $scope.message = "Lista"
+
+    $scope.contatos = JSON.parse(window.localStorage.getItem("contatos")) || [];
+
+    $scope.criterio = 'nome';
+    $scope.direcao = true;
+
+    $scope.operadoras = [
+        {nome : "Tim", codigo : "41", categoria: "Celular"},
+        {nome : "Oi", codigo : "14", categoria: "Celular"},
+        {nome : "Vivo", codigo : "15", categoria: "Celular"},
+        {nome : "Nextel", codigo : "15", categoria: "Fixo"},
+        {nome : "Embratel", codigo : "15", categoria: "Fixo"}
+    ];
+
+    $scope.adicionar = function (contato){
+        
+        $scope.contatos.push(angular.copy(contato));
+        window.localStorage.setItem("contatos", JSON.stringify($scope.contatos));
+        delete $scope.contato;
+        $scope.contatoFrm.$setPristine();
+    }
+    $scope.apagar = function (contatos){
+        $scope.contatos = contatos.filter(function(contato){
+            if(!contato.selecionado) return contato;
+        });
+        window.localStorage.setItem("contatos", JSON.stringify($scope.contatos));
+    }
+
+    $scope.isSelecionado = function(contatos){
+        return contatos.some(function (contato){
+            return contato.selecionado
+        });
+    }
+
+    $scope.odernarPor = function(campo){
+        $scope.criterio = campo;
+        $scope.direcao = !$scope.direcao;
+    }
+
+
+});
